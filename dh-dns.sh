f11ed7de0bd7049c3aa4d769e555205f616e4b48
toddrob <toddrob@gmail.com>
2018-05-22 17:46:32 -0400

    v1.1
    * Check individual domains even if last known IP matches newly-detected
    IP. That way if an update failed last time around, it will try again.
    * Improved logging
    * Added dh-dns.sh to run as a daemon on Linux (I use it on Synology)

---
+++ dh-dns.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+cd /path/to/dh-dns-master/
+# Check if already running, if not then run
+if  ps ax | grep -v grep | grep -q /path/to/dh-dns-master/dh-dns.py; then
+    :            # do nothing if already running
+else
+	nohup python -u /path/to/dh-dns-master/dh-dns.py &
+fi
+exit 0
