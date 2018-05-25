d69f2712a986f131ad33fdac79e63039fc0e02d1
toddrob <toddrob@gmail.com>
2018-05-25 18:56:48 -0400

    v1.2
    * Add `IPy` dependency install to `dh-dns.sh` startup script

--- dh-dns.sh
+++ dh-dns.sh
@@ -4,6 +4,7 @@ cd /path/to/dh-dns-master/
 if  ps ax | grep -v grep | grep -q /path/to/dh-dns-master/dh-dns.py; then
     :            # do nothing if already running
 else
+	easy_install IPy
 	nohup python -u /path/to/dh-dns-master/dh-dns.py &
 fi
 exit 0
